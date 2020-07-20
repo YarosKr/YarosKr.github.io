fc72c767107eee267ad9ec76336e623a3861a374
YarosKr <62641718+YarosKr@users.noreply.github.com>
2020-07-20 13:44:13 +0300

    send

---
+++ obsach/send.php
@@ -0,0 +1,17 @@
+<?php
+$fio = $_POST['username'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("yarosfkr@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: ydemov1@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
